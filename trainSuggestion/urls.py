28ec59f35d4aaddd19eb936f7bf0cca97dc6d378
thandepzai <cuthan2k1@gmail.com>
2023-10-31 22:52:13 +0700

    fix some

--- trainSuggestion/urls.py
+++ trainSuggestion/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
@@ -22,7 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('suggestProducts.urls')),
-    path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
